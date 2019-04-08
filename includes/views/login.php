78d90b14fb9c4881dd20c96b58d711280e90fb7e
Manuel Seisl <1810653153@stud.fh-kufstein.ac.at>
2019-04-08 12:41:00 +0200

    adjust login and register view

--- includes/views/login.php
+++ includes/views/login.php
@@ -1,21 +1,31 @@
 <?php
     echo $this->header;
 ?>
-<div class="main">
-    <div class="login">
+<div class="main login">
+    <div class="container">
         <form class="form-signin">
-            <h1 class="h3 mb-3 font-weight-normal">Login</h1>
-            <label class="sr-only" for="email" >E-Mail Adresse</label>
-            <input class="form-control" type="email" id="email"  placeholder="E-Mail Adresse" required autofocus>
-            <label class="sr-only" for="password">Passwort</label>
-            <input class="form-control" type="password" id="password" placeholder="Passwort" required>
-            <div class="checkbox mb-3">
+            <div class="row mb-2">
+                <h1 class="h3 d-block font-weight-normal text-center w-100">Filemanager</h1>
+            </div>
+            <div class="row mb-1">
+                <label class="sr-only" for="email" >E-Mail Adresse</label>
+                <input class="form-control" type="email" id="email" placeholder="E-Mail Adresse" required autofocus>
+            </div>
+            <div class="row mb-2">
+                <label class="sr-only" for="password">Passwort</label>
+                <input class="form-control" type="password" id="password" placeholder="Passwort" required>
+            </div>
+            <div class="row mb-2 justify-content-center">
                 <label>
                     <input type="checkbox" value="remember-me"> Angemeldet bleiben
                 </label>
             </div>
-            <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
-            <a href="<?php echo URL_PATH; ?>/register" class="btn btn-lg btn-secondary btn-block" id="register">Register</a>
+            <div class="row mb-2">
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+            </div>
+            <div class="row">
+                <a href="<?php echo URL_PATH; ?>/register" class="btn btn-lg btn-secondary btn-block" id="register">Register</a>
+            </div>
         </form>
     </div>
 </div>
--- includes/views/register.php
+++ includes/views/register.php
@@ -1,14 +1,31 @@
 <?php
     echo $this->header;
 ?>
-<div class="main">
-    <div class="register">
+<div class="main register">
+    <div class="container">
         <form class="form-signup">
-            <h1 class="h2 mb-3 font-weight-normal"> Register now</h1>
-            <p> Erstelle jetzt einen Account. Kostenlos & unverbindlich!</p>
-            <input class="form-control" type="text" id="fname" placeholder="Vorname">
-            <input class="form-control" type="text" id="lname" placeholder="Nachname">
-            <input class="form-control" type="email" id="email" placeholder="E-Mail Adresse">
+            <div class="row mb-1">
+                <h1 class="h2 font-weight-normal text-center">Registrierung</h1>
+            </div>
+            <div class="row mb-3">
+                <p class="m-0">Erstellen Sie jetzt einen kostenlosen Account.</p>
+            </div>
+            <div class="row mb-1">
+                <label class="sr-only" for="salutation">Anrede</label>
+                <!--<input class="form-control" type="text" id="salutation" placeholder="Anrede" required autofocus>-->
+            </div>
+            <div class="row mb-1">
+                <label class="sr-only" for="firstname">Vorname</label>
+                <input class="form-control" type="text" id="firstname" placeholder="Vorname" required autofocus>
+            </div>
+            <div class="row mb-1">
+                <label class="sr-only" for="lastname">Nachname</label>
+                <input class="form-control" type="text" id="lastname" placeholder="Nachname" required autofocus>
+            </div>
+            <div class="row mb-1">
+                <label class="sr-only" for="email" >E-Mail Adresse</label>
+                <input class="form-control" type="email" id="email" placeholder="E-Mail Adresse" required autofocus>
+            </div>
             <input class="form-control" type="password" id="password" placeholder="Passwort">
             <input class="form-control" type="password" id="passwordRepeated" placeholder="Password bestätigen">
             <button class="btn btn-lg btn-primary btn-block" id="registerBtn" name="signup"> Abschicken</button>
